17c0c518e1d22a2194419551469e0553bd586bd3
seul-dev <seul.dev@gmail.com>
2022-11-10 00:19:40 +0900

    style: globalstyle 적용

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  color: {
+    black: "#000000",
+    gray: "#c2c8cd",
+    blue: "#007BE9",
+    skyblue: "#cae9ff",
+    white: "#FFFFFF"
+  }
+};
