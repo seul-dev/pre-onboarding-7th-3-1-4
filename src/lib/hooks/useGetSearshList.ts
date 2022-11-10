4cd14d7c4caea7111fe7bdab6d317d56f4c3814e
seul-dev <seul.dev@gmail.com>
2022-11-10 14:26:21 +0900

    feat: api data 캐싱 추가

--- src/lib/hooks/useGetSearshList.ts
+++ src/lib/hooks/useGetSearshList.ts
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { getSearchResultsService } from "../api";
 import { searchKeyword } from "../states/searchKeyword";
 import { searchResults } from "../states/searchResults";
 import { inputValidation } from "../util/inputValidation";
+import { ICache } from "../../types/types";
 
 const useGetSearshList = () => {
   const inputValue = useRecoilValue(searchKeyword);
   const [searchResultsList, setSearchResultsList] =
     useRecoilState(searchResults);
-  //   const [cache, setCache] = useState({});
+  const [cache, setCache] = useState<ICache>({});
 
   const getSearchResults = async (keyword: string) => {
-    const data = await getSearchResultsService.search(keyword);
-    setSearchResultsList(data);
+    if (!cache[keyword]) {
+      const data = await getSearchResultsService.search(keyword);
+      setSearchResultsList(data);
+      setCache((prev) => ({ ...prev, [keyword]: data }));
+    }
+    if (cache[keyword]) {
+      setSearchResultsList(cache[keyword]);
+    }
   };
   useEffect(() => {
     if (inputValidation(inputValue)) {
--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,7 @@ export interface IDisease {
 }
 
 export type SearchResponse = Promise<IDisease[]>;
+
+export interface ICache {
+  [key: string]: IDisease[];
+}
